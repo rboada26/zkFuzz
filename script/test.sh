3c6609fa91b708c4b692a59122d65aa75f61cb1f
Koukyosyumei <koukyosyumei@hotmail.com>
2024-10-21 09:08:23 -0400

    update

--- script/test.sh
+++
@@ -1,4 +0,0 @@
-mkdir build
-
-circom test/matrix.t.circom --r1cs --wasm --sym --c -o build
-node build/matrix.t_js/generate_witness.js build/matrix.t_js/matrix.t.wasm test/matrix.t.json build/matrix.t.wtns
