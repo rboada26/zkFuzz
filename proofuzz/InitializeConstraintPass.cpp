ef116ed6f4da527599a2fec408c35b382db30986
Koukyosyumei <koukyosyumei@hotmail.com>
2024-10-23 15:52:03 -0400

    update

--- proofuzz/InitializeConstraintPass.cpp
+++ proofuzz/InitializeConstraintPass.cpp
@@ -7,11 +7,26 @@ using namespace llvm;
 
 namespace
 {
+    /**
+     * @brief LLVM ModulePass that initializes the external global variable `@constraint`
+     * to `false`. This pass looks for a global variable named `constraint`, which is
+     * assumed to be a boolean (i1 type), and sets its value to `false` (0).
+     */
     struct InitializeConstraintPass : public ModulePass
     {
         static char ID;
         InitializeConstraintPass() : ModulePass(ID) {}
 
+        /**
+         * @brief Runs the transformation pass on the given LLVM module.
+         *
+         * This method iterates over all global variables in the module and identifies
+         * the one named `constraint`. If found and it is a declaration (without an initializer),
+         * the pass sets its linkage to `InternalLinkage` and initializes it to `false`.
+         *
+         * @param M The LLVM module to be transformed.
+         * @return true Always returns true to indicate the module was modified.
+         */
         bool runOnModule(Module &M) override
         {
             // Modify external global variable `@constraint` to initialize with `false`.
